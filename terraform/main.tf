5119541d334f0ae868a3c22e465c728cb7821213
RadostSh <r.shopovaa@gmail.com>
2026-01-04 00:11:15 +0200

    add Terraform IaC for Kubernetes deployment

---
+++ terraform/main.tf
@@ -0,0 +1,78 @@
+terraform {
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.23"
+    }
+  }
+}
+
+provider "kubernetes" {
+  config_path    = "~/.kube/config"
+  config_context = "docker-desktop" # Увери се, че това е името на контекста ти
+}
+
+resource "kubernetes_namespace" "app_ns" {
+  metadata {
+    name = "slack-bot-iac"
+  }
+}
+
+resource "kubernetes_deployment" "slack_bot" {
+  metadata {
+    name      = "slack-bot-deployment"
+    namespace = kubernetes_namespace.app_ns.metadata[0].name
+    labels = {
+      app = "slack-bot"
+    }
+  }
+
+  spec {
+    replicas = 1 # Един бот е достатъчен
+
+    selector {
+      match_labels = {
+        app = "slack-bot"
+      }
+    }
+
+    template {
+      metadata {
+        labels {
+          app = "slack-bot"
+        }
+      }
+
+      spec {
+        container {
+          image = "slack-bot:latest"
+          name  = "slack-bot-container"
+          
+          image_pull_policy = "Never" 
+
+          port {
+            container_port = 8000
+          }
+
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_service" "slack_bot_service" {
+  metadata {
+    name      = "slack-bot-service"
+    namespace = kubernetes_namespace.app_ns.metadata[0].name
+  }
+  spec {
+    selector = {
+      app = kubernetes_deployment.slack_bot.metadata[0].labels.app
+    }
+    port {
+      port        = 8000
+      target_port = 8000
+    }
+    type = "LoadBalancer"
+  }
+}
